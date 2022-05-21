ed26cf77ba8828dc90f304b18db4093e31df462e
Francis Beeson <leobeeson@gmail.com>
2022-05-21 14:32:28 +0100

    Access jobs page

--- main.py
+++ main.py
@@ -3,23 +3,38 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
-## Setup chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless") # Ensure GUI is off
-chrome_options.add_argument("--no-sandbox")
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+ENTITY = os.environ.get("ENTITY")
+OTHER = os.environ.get("OTHER")
+
 
 # Set path to chromedriver as per your configuration.git
-webdriver_service = Service("/usr/local/bin/chromedriver/stable/chromedriver")
+webdriver_service = Service("C:\Drivers\chromedriver.exe")
 
 # Choose Chrome Browser
-driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+driver = webdriver.Chrome(service=webdriver_service)
 
 # Get page
-driver.get("https://www.linkedin.com/jobs/")
+# driver.get("https://www.linkedin.com/jobs/")
+# Remote/Scotland/python%20developer
+driver.get("https://www.linkedin.com/jobs/search/?f_AL=true&f_WT=2&geoId=100752109&keywords=python%20developer&location=Scotland%2C%20United%20Kingdom")
 
-recommended = driver.find_element(By.CSS_SELECTOR, "#ember1541").get_attribute("href")
-print(f"{recommended}")
+log_in = driver.find_element(By.LINK_TEXT, "Sign in")
+log_in.click()
 
-time.sleep(5)
+entity = driver.find_element(By.ID, "username")
+entity.send_keys(ENTITY)
+
+other = driver.find_element(By.ID, "password")
+other.send_keys(OTHER)
+other.send_keys(Keys.ENTER)
+
+
+time.sleep(60)
 driver.quit()
