d99de06d9e745197dbc253f92f0971f6ee377666
Francis Beeson <leobeeson@gmail.com>
2022-05-26 23:50:22 +0100

    Add pagination

--- main.py
+++ main.py
@@ -52,7 +52,10 @@ keyword_url_safe = quote_plus(keyword_argument)
 location_argument = "scotland, united kingdom"
 location_url_safe = quote_plus(location_argument)
 
-driver.get(f"https://www.linkedin.com/jobs/search/?f_TPR=r604800&f_WT=2%2C3&keywords={keyword_url_safe}&location={location_url_safe}")
+full_url = f"https://www.linkedin.com/jobs/search/?f_TPR=r604800&f_WT=2%2C3&keywords={keyword_url_safe}&location={location_url_safe}"
+
+driver.get(full_url)
+print(f"full_url: {full_url}")
 
 log_in = driver.find_element(By.LINK_TEXT, "Sign in")
 log_in.click()
@@ -66,41 +69,53 @@ other.send_keys(Keys.ENTER)
 
 driver.maximize_window()
 
-listings = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")
-num_listings = len(listings)
-print(f"Nunber of listings: {num_listings}")
 
+pages = driver.find_elements(By.CLASS_NAME, "artdeco-pagination__indicator")
+print(f"Number of Pages: {len(pages)}")
 
 listings_data = []
-for idx, listing in enumerate(listings):
-    listing_data = {}
-    listing_data["results_position"] = idx
 
-    listing = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")[idx]
-    time.sleep(0.5)
-    listing.click()
-    time.sleep(0.5)
+for page_num in range(1, len(pages) + 1):
+    print(f"Page Number: {page_num}")
+    listings = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")
+    num_listings = len(listings)
+    print(f"Nunber of listings: {num_listings}")
     
-    job_id = listing.get_attribute("data-occludable-job-id")
-    listing_data["job_id"] = job_id
+    for idx, listing in enumerate(listings):
+        listing_data = {}
+        listing_data["page_number"] = page_num
+        listing_data["results_position"] = idx
 
-    job_title_element = wait_for_element(listing, "job-card-list__title")
-    job_title = job_title_element.text
-    listing_data["job_title"] = job_title
+        listing = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")[idx]
+        time.sleep(0.5)
+        listing.click()
+        time.sleep(0.5)
+        
+        job_id = listing.get_attribute("data-occludable-job-id")
+        listing_data["job_id"] = job_id
 
-    job_url = job_title_element.get_attribute("href")
-    listing_data["job_url"] = job_url
-    
-    company_name_element = wait_for_element(listing, "job-card-container__company-name")
-    company_name = company_name_element.text
-    listing_data["company_name"] = company_name
-    
-    job_description_element = wait_for_element(driver, "jobs-description-content__text")
-    job_description = job_description_element.text
-    listing_data["job_description"] = job_description
-    
-    listings_data.append(listing_data)
+        job_title_element = wait_for_element(listing, "job-card-list__title")
+        job_title = job_title_element.text
+        listing_data["job_title"] = job_title
 
+        job_url = job_title_element.get_attribute("href")
+        listing_data["job_url"] = job_url
+        
+        company_name_element = wait_for_element(listing, "job-card-container__company-name")
+        company_name = company_name_element.text
+        listing_data["company_name"] = company_name
+        
+        job_description_element = wait_for_element(driver, "jobs-description-content__text")
+        job_description = job_description_element.text
+        listing_data["job_description"] = job_description
+        
+        listings_data.append(listing_data)
+
+    if page_num < len(pages):
+        page_xpath = f"/html/body/div[7]/div[3]/div[3]/div[2]/div/section[1]/div/div/section/div/ul/li[{page_num + 1}]/button"
+        pagination = driver.find_element(By.XPATH, page_xpath)
+        pagination.click()
+        time.sleep(5)
 
 date_prefix = time.strftime("%Y-%m-%d")
 
