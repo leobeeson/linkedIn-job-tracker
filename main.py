8259b3a1a61ba5b4566c97490f18f20da7587ce9
Francis Beeson <leobeeson@gmail.com>
2022-06-03 13:21:27 +0100

    Fix bug: go back when driver clicks url instead of element

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
 
 
 import os
@@ -38,6 +39,12 @@ def wait_for_element(driver: WebDriver, class_name: str) -> WebElement:
             )
     return element
 
+def click_on_pagination_number(page_number: int) -> None:
+    page_xpath = f"/html/body/div[7]/div[3]/div[3]/div[2]/div/section[1]/div/div/section/div/ul/li[{page_number}]/button"
+    pagination = driver.find_element(By.XPATH, page_xpath)
+    pagination.click()
+    time.sleep(5)
+
 
 # Set path to chromedriver as per your configuration.git
 webdriver_service = Service("C:\Drivers\chromedriver.exe")
@@ -56,6 +63,8 @@ full_url = f"https://www.linkedin.com/jobs/search/?f_TPR=r604800&f_WT=2%2C3&keyw
 
 driver.get(full_url)
 print(f"full_url: {full_url}")
+
+time.sleep(2)
 
 log_in = driver.find_element(By.LINK_TEXT, "Sign in")
 log_in.click()
@@ -79,43 +88,73 @@ for page_num in range(1, len(pages) + 1):
     print(f"Page Number: {page_num}")
     listings = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")
     num_listings = len(listings)
-    print(f"Nunber of listings: {num_listings}")
+    print("Number of listings: {num_listings}")
     
+    current_page_url = full_url
+
     for idx, listing in enumerate(listings):
+        print(f"Listing number: {page_num}-{idx}")
         listing_data = {}
         listing_data["page_number"] = page_num
         listing_data["results_position"] = idx
 
-        listing = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")[idx]
-        time.sleep(0.5)
-        listing.click()
-        time.sleep(0.5)
+        print(f"Get listing element: jobs-search-results__list-item")
+        try:
+            listing = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")[idx]
+            time.sleep(0.5)
+            listing.click()
+            time.sleep(0.5)
+        except IndexError:
+            driver.back() 
+            listing = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")[idx]
+            time.sleep(0.5)
+            listing.click()
+            time.sleep(0.5)
         
-        job_id = listing.get_attribute("data-occludable-job-id")
-        listing_data["job_id"] = job_id
+        print(f"Get job id: data-occludable-job-id")
+        try:
+            job_id = listing.get_attribute("data-occludable-job-id")
+            listing_data["job_id"] = job_id
+        except (StaleElementReferenceException, TimeoutException):
+            listing_data["job_id"] = None
 
-        job_title_element = wait_for_element(listing, "job-card-list__title")
-        job_title = job_title_element.text
-        listing_data["job_title"] = job_title
+        print(f"Get job title: job-card-list__title")
+        try:
+            job_title_element = wait_for_element(listing, "job-card-list__title")
+            job_title = job_title_element.text
+            listing_data["job_title"] = job_title
+        except (StaleElementReferenceException, TimeoutException):
+            listing_data["job_title"] = None
 
-        job_url = job_title_element.get_attribute("href")
-        listing_data["job_url"] = job_url
+        print(f"Get job url: href")
+        try:
+            job_url = job_title_element.get_attribute("href")
+            listing_data["job_url"] = job_url
+        except (StaleElementReferenceException, TimeoutException):
+            listing_data["job_url"] = None
         
-        company_name_element = wait_for_element(listing, "job-card-container__company-name")
-        company_name = company_name_element.text
-        listing_data["company_name"] = company_name
-        
-        job_description_element = wait_for_element(driver, "jobs-description-content__text")
-        job_description = job_description_element.text
-        listing_data["job_description"] = job_description
+        print(f"Get company name: job-card-container__company-name")
+        try:
+            company_name_element = wait_for_element(listing, "job-card-container__company-name")
+            company_name = company_name_element.text
+            listing_data["company_name"] = company_name
+        except (StaleElementReferenceException, TimeoutException):
+            listing_data["company_name"] = None
+
+        print(f"Get job description: jobs-description-content__text")
+        try:        
+            job_description_element = wait_for_element(driver, "jobs-description-content__text")
+            job_description = job_description_element.text
+            listing_data["job_description"] = job_description
+        except (StaleElementReferenceException, TimeoutException):
+            listing_data["job_description"] = None
         
         listings_data.append(listing_data)
 
     if page_num < len(pages):
-        page_xpath = f"/html/body/div[7]/div[3]/div[3]/div[2]/div/section[1]/div/div/section/div/ul/li[{page_num + 1}]/button"
-        pagination = driver.find_element(By.XPATH, page_xpath)
-        pagination.click()
-        time.sleep(5)
+        click_on_pagination_number(page_num + 1)
+        current_page_url = driver.current_url
+
 
 date_prefix = time.strftime("%Y-%m-%d")
 
